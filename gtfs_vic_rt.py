3deb37b386899586e40d0d1896eafdf3919fa4b2
jozwang <9334262+jozwang@users.noreply.github.com>
2025-07-14 22:23:48 +1000

    Update gtfs_vic_rt.py

--- gtfs_vic_rt.py
+++ gtfs_vic_rt.py
@@ -31,7 +31,7 @@ if response.status_code == 200:
         if entity.HasField('trip_update'):
             trip = entity.trip_update.trip
             vehicle = entity.trip_update.vehicle
-            route= trip.route
+            route= trip.route_id
              
             records.append({
                 "Vehicle ID": vehicle.id if vehicle and vehicle.id else "N/A",
